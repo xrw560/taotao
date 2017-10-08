6607464b9800058a8f4e5cea935a0aa22a6219b2
MicroWin10-1512\Administrator <ncutits@163.com>
2017-10-08 22:04:06 +0800

    大广告位

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,17 +1,25 @@
 package com.taotao.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.portal.service.ContentService;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	private ContentService contentService;
+
 	@RequestMapping("/index")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		String json = contentService.getContentList();
+		model.addAttribute("ad1", json);
 		return "index";
 	}
 
